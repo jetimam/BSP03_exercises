c0b5b0c4d461036296003f6317264a62c13fb302
Matt Umut Ucak <48724128+jetimam@users.noreply.github.com>
2021-10-07 22:11:29 +0200

    Romania problem has been initiated.
    The map's representation has been implemented.

---
+++ Romania/Neighbor.java
@@ -0,0 +1,26 @@
+public class Neighbor
+{
+	private String cityName;
+	private int cityDistance;
+
+	public Neighbor(String cityName, int cityDistance)
+	{
+		this.cityName = cityName;
+		this.cityDistance = cityDistance;
+	}
+
+	public String toString()
+	{
+		return cityName + " - " + cityDistance;
+	}
+
+	public String getCityName()
+	{
+		return this.cityName;
+	}
+
+	public int getCityDistance()
+	{
+		return this.cityDistance;
+	}
+}
